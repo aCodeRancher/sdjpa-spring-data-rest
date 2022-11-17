0eff2456e617c7053d4b5fca81b48596f7450983
helen <hhma@alumni.cpp.edu>
2022-11-17 17:18:42 -0500

    add Customer Entity for experiment.

--- src/main/java/guru/springframework/sfgrestbrewery/domain/Customer.java
+++ src/main/java/guru/springframework/sfgrestbrewery/domain/Customer.java
@@ -1,9 +1,15 @@
 package guru.springframework.sfgrestbrewery.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import java.util.UUID;
 
@@ -14,7 +20,14 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
+
     private String name;
 }
